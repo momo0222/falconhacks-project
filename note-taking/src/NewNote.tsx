a126d33117ca5398dfcac41101e4dc13e8dc4eb4
momo0222 <joywang0222@gmail.com>
2023-12-09 10:29:45 -0800

    initial

---
+++ note-taking/src/NewNote.tsx
@@ -0,0 +1,13 @@
+import { NoteForm } from "./NoteForm"
+import { NoteData } from "./App"
+type NewNoteProps = {
+    onSubmit: (data: NoteData) => void
+}
+export function NewNote({onSubmit}: NewNoteProps){
+    return(
+        <>
+        <h1 className="mb-4">New Note</h1>
+        <NoteForm onSubmit={onSubmit}/>
+        </> 
+    )
+}
